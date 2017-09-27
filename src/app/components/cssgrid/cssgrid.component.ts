278459f5e6d21a77a34fdd6a7c6acd4a486dd743
Senthil Kumaran <ursenix@gmail.com>
2017-09-27 12:12:41 +0530

    models folder added

--- src/app/components/cssgrid/cssgrid.component.ts
+++ src/app/components/cssgrid/cssgrid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IContact} from './models/IContact';
 
 @Component({
   selector: 'app-cssgrid',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cssgrid.component.css']
 })
 export class CssgridComponent implements OnInit {
+
+  
 
   constructor() { }
 
